7ab5dd84c36309049e53d25257343d8465802791
Udayteja189 <udaytejachary@gmail.com>
2024-08-25 11:07:35 +0530

    add sonner styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./App.css";
-import Login from "./components/organisms/Login";
 import Home from "./components/organisms/Home";
-import Signup from "./components/organisms/Signup";
 import { Route, Routes } from "react-router-dom";
 import Wishlist from "./components/organisms/Wishlist";
 import Cart from "./components/organisms/Cart";
 import IndividualProduct from "./components/molecules/IndividualProduct";
+import { Toaster } from "sonner";
 
 function App() {
   return (
     <div className="App">
+      <Toaster position="bottom-right" richColors closeButton />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="product/:id" element={<IndividualProduct/>} />
